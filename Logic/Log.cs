d52ad6b3acb0cff8f3002607d6a5c0dc978bfdd1
MoriYorr <yourmori1@gmail.com>
2025-04-17 22:00:57 +0500

    Add files via upload

---
+++ Logic/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    class Log
+    {
+        public void CreateFile()
+        {
+            if (!File.Exists("Log.txt"))
+            {
+                FileStream logFile = File.Create("Log.txt");
+                logFile.Close();
+            }
+        }
+
+        public void WriteMessage(string message)
+        {
+            StreamWriter logWrite = new("Log.txt", true);
+            logWrite.WriteLine("[" + DateTime.Now.ToString() + "] " + message);
+            logWrite.Close();
+        }
+    }
+}
---
+++ Logic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    //ну и что за дела, чего не работаем?
+    internal class CreateFile
+    {
+        static void Main(string name, string format)
+        {
+            string fullName = name + format;
+
+            try
+            {
+                using (FileStream fs = File.Create(fullName))
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
